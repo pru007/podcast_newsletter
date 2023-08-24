342513b00ca96fd8ff1839d81acad9ceb982c03b
pru007 <a.p.kprudhvi007@gmail.com>
2023-08-24 18:16:44 +0530

    Add files via upload

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -4,7 +4,7 @@ import json
 import os
 
 def main():
-    st.title("Newsletter Dashboard")
+    st.title("Closets By Design, Podcast Summarizer Demo")
 
     available_podcast_info = create_dict_from_json_files('.')
 
